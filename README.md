a758a8d829435e61a3a3e67a03ae1b088c1d254a
Nicolas Krul <nicolaskrul@gmail.com>
2025-03-13 20:03:33 +0100

    template generation for nodes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# highglade
+# Highglade
+
+## Dependencies:
+
+### Golang
+```sh
+brew install go
+```
+
+### mage 
+```sh
+brew install mage
+```
+### TalosCTL
+```sh
+brew install talosctl
+```
--- scripts/generate-node-configs.py
+++ scripts/generate-node-configs.py
@@ -4,11 +4,11 @@ import os
 from jinja2 import Template
 
 print(os.getcwd())
+with open(os.getcwd() + "/cluster/main/general.yaml") as file:
+    general = yaml.safe_load(file)
+
 with open(os.getcwd() + "/cluster/main/machines.yaml") as file:
     machines = yaml.safe_load(file)
-
-with open(os.getcwd() + "/cluster/main/network.yaml") as file:
-    network = yaml.safe_load(file)
 
 # read your jinja template file
 # this is just a modified controleplane.yml file
@@ -20,9 +20,10 @@ with open(os.getcwd() + "/cluster/main/machine-template.yaml.j2") as file:
 for machine in machines["machines"]:
     print(machine["name"])
     result = template.render(
-        machine=machine["name"],
-        network=network["general"],
-        dns=network["dns"],
+        general=general["general"],
+        machine=machine,
+        network=general["general"]["network"],
+        dns=general["general"]["dns"],
     )
     with open(
         os.getcwd() + "/cluster/main/talos/" + machine["name"] + ".yaml", "w"
